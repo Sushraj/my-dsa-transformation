5de9bdf59648bfc7bb97163dd7e51aa047aa507e
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-01-30 10:03:07 +0530

    warm up 1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- 
+// write a function that searches for an element in an array
+// and returns its index. If the element is not found, return -1.
+
+let arr = [10, 45, 15, 25, 20];
+
+function searchIndex(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) return i;
+  }
+  return -1;
+}
+
+console.log(searchIndex(arr, 15));
