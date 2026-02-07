160c7951f3f20421d38194f98db7d2839e24d7c1
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-02-08 00:31:58 +0530

    loop

--- problems/warm-up/loopinsideloop.js
+++ problems/warm-up/loopinsideloop.js
@@ -1,16 +1,16 @@
-function loops1() {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      console.log("i", i, "=", "j", j);
-    }
-  }
-}
+// function loops1() {
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       console.log("i", i, "=", "j", j);
+//     }
+//   }
+// }
 //loops();
 
 function loops2() {
   for (let i = 5; i > 0; i--) {
     for (let j = 0; j < i; j++) {
-      console.log("i", i, "=", "j", j);
+      console.log("i =" + i + "j =" + j);
     }
   }
 }
