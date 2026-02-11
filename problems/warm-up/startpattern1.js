70023849db51eb49b05d9839389be22f5cf749e5
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-02-11 23:53:27 +0530

    star pattern1

---
+++ problems/warm-up/startpattern1.js
@@ -0,0 +1,12 @@
+function starPattern() {
+  for (let i = 0; i < 4; i++) {
+    let row = " ";
+
+    for (let j = 0; j < 4; j++) {
+      row = row + "*";
+    }
+    console.log(row);
+  }
+}
+
+starPattern();
